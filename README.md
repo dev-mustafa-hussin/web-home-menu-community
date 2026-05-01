9bbebe753ffe9c740da4f866fce448f3c32145d5
Mustafa Hussein Ahmed <dev-mustafa-hussin@hotmail.com>
2026-05-01 20:27:01 +0300

    Initial commit: web home menu community module

---
+++ README.md
@@ -0,0 +1,27 @@
+# Web Home Menu Community
+
+Home Menu Module for Odoo Community Edition
+
+Replaces the default backend menu with a grid-based dashboard similar to Odoo Enterprise.
+
+## Features
+
+- Grid-based home menu
+- App cards with icons and names
+- Compatible with Odoo 19 Community
+
+## Installation
+
+1. Copy the module to your Odoo addons directory
+2. Update the module list
+3. Install the module from Apps
+
+## Author
+
+**3M Code Software**  
+Website: https://github.com/dev-mustafa-hussin  
+Support: support@3mcode.com
+
+## License
+
+LGPL-3
---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "Web Home Menu",
+    "version": "19.0.1.0.0",
+    "summary": "Home Menu for Odoo Community",
+    "description": """
+Home Menu Module for Odoo Community Edition
+Replaces the default backend menu with a grid-based dashboard similar to Odoo Enterprise
+    """,
+    "author": "3M Code Software",
+    "website": "https://github.com/dev-mustafa-hussin/web-home-menu-community",
+    "license": "LGPL-3",
+    "category": "Web",
+    "support": "support@3mcode.com",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/home_page.xml",
+        "security/groups.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "web_home_menu_community/static/src/xml/home_menu.xml",
+        ],
+    },
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
